5bd6fd3b3feb06926066b729be327db7dce5e139
Christopher Danan <chrisddan@gmail.com>
2015-03-21 02:43:47 -0700

    Add buttons to the html page that correspond to the five choices the player can make in the game (rock, paper, scissors, lizard, spock).

--- Client/Javascripts/client.js
+++ Client/Javascripts/client.js
@@ -13,7 +13,43 @@
 var main = function(){
 	"use strict";
 
-	console.log("Hello Vane!");
+	//console.log("Hello Vane!");
+
+	//Create variable names for the five buttons.
+	var $rock = $("<button>").text("Rock").addClass("rockBtn"),
+		$paper = $("<button>").text("Paper").addClass("paperBtn"),
+		$scissors = $("<button>").text("Scissors").addClass("scissorsBtn"),
+		$lizard = $("<button>").text("Lizard").addClass("lizardBtn"),
+		$spock = $("<button>").text("Spock").addClass("spockBtn");
+
+	//Append the five buttons to the html page.
+	$(".playerChoice").append($rock);
+	$(".playerChoice").append($paper);
+	$(".playerChoice").append($scissors);
+	$(".playerChoice").append($lizard);
+	$(".playerChoice").append($spock);
+
+	//Create button handlers for the five buttons.
+	$rock.on("click", function(){
+		console.log("rock button clicked");
+	});
+
+	$paper.on("click", function(){
+		console.log("paper button clicked");
+	});
+
+	$scissors.on("click", function(){
+		console.log("scissors button clicked");
+	});
+
+	$lizard.on("click", function(){
+		console.log("lizard button clicked");
+	});
+
+	$spock.on("click", function(){
+		console.log("spock button clicked");
+	});
+
 };
 
 $(document).ready(main);
