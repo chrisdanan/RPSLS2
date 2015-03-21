798b3c5bda705de1d024ac86439f8440a7981445
Christopher Danan <chrisddan@gmail.com>
2015-03-21 03:09:26 -0700

    Create routes that will handle posts from the buttons. Add body-parser so that it is easier to parse through incoming JSON objects.

--- server.js
+++ server.js
@@ -13,14 +13,54 @@
 "use strict";
 
 var express = require("express"),
+	bodyParser = require("body-parser"),
 	http = require("http"),
-	app = express();
+	app = express(),
+	wins = 0,
+	losses = 0,
+	ties = 0;
 
 app.use(express.static(__dirname + "/Client"));
+app.use(bodyParser());
 
 http.createServer(app).listen(3000);
 console.log("Server listening on port 3000");
 
 app.get("/test", function(req, res){
 	res.send("You routed to the test page, here's a cookie");
+});
+
+app.post("/play/rock", function(req, res){
+	console.log("Server received post");
+	console.log(req.body);
+
+	res.end();
+});
+
+app.post("/play/paper", function(req, res){
+	console.log("Server received post");
+	console.log(req.body);
+
+	res.end();
+});
+
+app.post("/play/scissors", function(req, res){
+	console.log("Server received post");
+	console.log(req.body);
+
+	res.end();
+});
+
+app.post("/play/lizard", function(req, res){
+	console.log("Server received post");
+	console.log(req.body);
+
+	res.end();
+});
+
+app.post("/play/spock", function(req, res){
+	console.log("Server received post");
+	console.log(req.body);
+
+	res.end();
 });
